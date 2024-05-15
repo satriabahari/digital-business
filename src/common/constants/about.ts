089897576c0fca9f5dc83acfe4fdf2b597331637
Satria Bahari <satriaabaharii@gmail.com>
2024-05-15 11:09:59 +0700

    feat: add some feature

---
+++ src/common/constants/about.ts
@@ -0,0 +1,5 @@
+export const ABOUT = [
+  "Lorem ipsum dolor sit amet consectetur adipisicing elit. Itaque, veniam totam doloribus, adipisci iusto earum quidem expedita eaque corrupti molestias distinctio soluta. Nam unde assumenda libero, veritatis dicta placeat aut repudiandae saepe, dolore labore officia molestiae illum velit laborum! Cum recusandae obcaecati cupiditate blanditiis a, accusamus voluptates hic culpa eveniet.",
+
+  "Lorem ipsum dolor sit amet consectetur adipisicing elit. Itaque, veniam totam doloribus, adipisci iusto earum quidem expedita eaque corrupti molestias distinctio soluta. Nam unde assumenda libero, veritatis dicta placeat aut repudiandae saepe, dolore labore officia molestiae illum velit laborum! Cum recusandae obcaecati cupiditate blanditiis a, accusamus voluptates hic culpa eveniet.",
+];
---
+++ src/common/constants/menu.tsx
@@ -0,0 +1,22 @@
+export const MENUS = [
+  {
+    name: "Home",
+    href: "/",
+    isShow: true,
+  },
+  {
+    name: "About",
+    href: "#about",
+    isShow: true,
+  },
+  {
+    name: "Courses",
+    href: "#courses",
+    isShow: true,
+  },
+  {
+    name: "Contact",
+    href: "#contact",
+    isShow: true,
+  },
+];
