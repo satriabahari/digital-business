a96180cfdb7a0e80869549a925a6952add72f1c6
Satria Bahari <satriaabaharii@gmail.com>
2024-05-19 17:26:50 +0700

    feat: create testimonial feature

--- src/components/testimonials/TestimonialCard.tsx
+++ src/components/testimonials/TestimonialCard.tsx
@@ -1,57 +1,27 @@
 import { cn } from "@/lib/utils.ts";
-import {
-  Card,
-  CardContent,
-  CardFooter,
-  CardHeader,
-  CardTitle,
-} from "../ui/card.tsx";
-import { FaStar as StarIcon } from "react-icons/fa";
+import type { TestimonialProps } from "@/common/types/testimonial.ts";
 
-interface TestimonialCardProps {
-  name: string;
-  course: string;
-  testimonial: string;
-  rating: number;
-  date: string;
-  col_span: boolean;
-}
+import { Card, CardContent, CardFooter, CardHeader, CardTitle } from "../ui/card.tsx";
 
-const TestimonialCard = ({
-  name,
-  course,
-  testimonial,
-  rating,
-  date,
-  col_span,
-}: TestimonialCardProps) => {
+const TestimonialCard = ({ name, course, testimonial, rating, col_span }: TestimonialProps) => {
   const generateStars = (rating: number) => {
     return Array(rating).fill("⭐").join("");
   };
 
   return (
     <Card
-      className={cn(
-        "border-[1.5px] dark:border-neutral-500 rounded-xl dark:bg-neutral-800",
-        col_span && "lg:col-span-2"
-      )}
+      className={cn("rounded-xl border-[1.5px] dark:border-primary dark:bg-dark-tertiary", col_span && "lg:col-span-2")}
     >
       <CardHeader>
-        <CardTitle className="dark:text-neutral-50 text-xl">{course}</CardTitle>
+        <CardTitle className="text-xl dark:text-neutral-50">{course}</CardTitle>
       </CardHeader>
       <CardContent>
         <h2 className="dark:text-neutral-400">"{testimonial}"</h2>
       </CardContent>
       <CardFooter className="flex justify-between">
-        <div className="flex gap-x-3 items-center">
-          <div className="p-[2.5px] border-[1.5px] border-neutral-700 rounded-full">
-            <img
-              src="/images.jpeg"
-              width={50}
-              height={50}
-              alt={name}
-              className="rounded-full object-cover w-8 h-8"
-            />
+        <div className="flex items-center gap-x-3">
+          <div className="rounded-full border-[1.5px] border-neutral-700 p-[2.5px] dark:border-primary">
+            <img src="/images.jpeg" width={50} height={50} alt={name} className="h-8 w-8 rounded-full object-cover" />
           </div>
           <p className="font-semibold dark:text-neutral-300">{name}</p>
         </div>
